d4424840833ab39b0606a69f16bf9bcd1d26117f
Meharoof Najeeb <meharoofnajeeb@gmail.com>
2025-08-29 19:39:15 -0400

    Initial Commit

---
+++ MoviesTR/MoviesTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTRApp.swift
+//  MoviesTR
+//
+//  Created by Meharoof Najeeb on 2025-08-29.
+//
+
+import SwiftUI
+
+@main
+struct MoviesTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

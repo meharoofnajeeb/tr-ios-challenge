24daaa9e47eeb823fc2b03b2630d208a5e506f44
Meharoof Najeeb <meharoofnajeeb@gmail.com>
2025-08-30 21:05:15 -0400

    Added Movie domain model.

---
+++ MoviesTR/Domain/Entities/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  MoviesTR
+//
+//  Created by Meharoof Najeeb on 2025-08-30.
+//
+
+import Foundation
+
+struct Movie: Identifiable {
+    let id: Int
+    let name: String
+    let imageURL: URL
+    let year: String
+}

9d9e1989dd7fc3428e908309806038abb7f81116
Meharoof Najeeb <meharoofnajeeb@gmail.com>
2025-09-01 12:25:37 -0400

    MovieDetailsViewModel.fetchMovieDetails` fetches movie details and sets the `MovieDetail` object, isLoading and errorMessage accordingly on success.

---
+++ MoviesTR/Presentation/MovieDetails/ViewModel/MovieDetailsViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  MovieDetailsViewModel.swift
+//  MoviesTR
+//
+//  Created by Meharoof Najeeb on 2025-09-01.
+//
+
+import Combine
+
+final class MovieDetailViewModel {
+    private let getMovieDetailsUseCase: GetMovieDetailsUseCaseProtocol
+    private let movieID: Int
+    
+    @Published var movieDetails: MovieDetail?
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String?
+    
+    init(getMovieDetailsUseCase: GetMovieDetailsUseCaseProtocol, movieID: Int) {
+        self.getMovieDetailsUseCase = getMovieDetailsUseCase
+        self.movieID = movieID
+    }
+    
+    func fetchMovieDetails() async {
+        isLoading = true
+        errorMessage = nil
+        do {
+            movieDetails = try await getMovieDetailsUseCase.getMovieDetails(for: movieID)
+        } catch {}
+        isLoading = false
+    }
+}
--- MoviesTRTests/Presentation/MovieDetails/ViewModel/MovieDetailsViewModelTests.swift
+++ MoviesTRTests/Presentation/MovieDetails/ViewModel/MovieDetailsViewModelTests.swift
@@ -6,8 +6,45 @@
 //
 
 import XCTest
+@testable import MoviesTR
 
 final class MovieDetailsViewModelTests: XCTestCase {
-
+    func test_fetchMovieDetails_fetchesMovieDetailsAndUpdatesViewModelStates() async {
+        let movieID = 1
+        let movieDetails = getAnyMovieDetail(with: movieID)
+        let getMovieDetailsUseCase = GetMovieDetailsUseCaseMock(movieDetails: movieDetails)
+        let sut = MovieDetailViewModel(getMovieDetailsUseCase: getMovieDetailsUseCase, movieID: movieID)
+        
+        await sut.fetchMovieDetails()
+        
+        XCTAssertEqual(getMovieDetailsUseCase.getMovieDetailsCallCount, 1)
+        XCTAssertFalse(sut.isLoading)
+        XCTAssertEqual(sut.movieDetails, movieDetails)
+        XCTAssertNil(sut.errorMessage)
+    }
     
+    //MARK: - Private helpers
+    private func getAnyMovieDetail(with id: Int) -> MovieDetail {
+        return MovieDetail(id: id,
+                           name: "Movie \(id)",
+                           description: "Some description for Movie \(id)",
+                           notes: "Some notes for Movie \(id)",
+                           rating: "10.0",
+                           imageURL: URL(string: "http://some-url.com")!,
+                           releaseDate: Date())
+    }
+    
+    private class GetMovieDetailsUseCaseMock: GetMovieDetailsUseCaseProtocol {
+        private(set) var getMovieDetailsCallCount = 0
+        var movieDetails: MovieDetail?
+        
+        init(movieDetails: MovieDetail? = nil) {
+            self.movieDetails = movieDetails
+        }
+        
+        func getMovieDetails(for id: Int) async throws -> MovieDetail {
+            getMovieDetailsCallCount += 1
+            return movieDetails!
+        }
+    }
 }
